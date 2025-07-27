d8971e509ba2bf9c692eeee48e2d54761a9a05d7
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-27 09:29:16 +0200

    Create User.js

---
+++ Servidor/models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  devices: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Device'
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  lastLogin: Date
+});
+
+// Hash da senha antes de salvar
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password') || this.isNew) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+// Método para comparar senhas
+userSchema.methods.comparePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
